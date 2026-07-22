13c99f490736e70ee1cc13a4af1b44034044488f
EnzoFelyx <enzofelyx@gmail.com>
2026-07-21 23:08:01 -0300

    feat: query and nativewind config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from "expo-router";
-import '../styles/global.css'
+import '../styles/global.css';
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
-    return <Stack/>
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Stack />
+        </QueryClientProvider>
+    )
 }

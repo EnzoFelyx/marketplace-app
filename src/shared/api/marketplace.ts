0b77d599e7b11196ac168b6450eb6696373636ba
EnzoFelyx <enzofelyx@gmail.com>
2026-08-09 22:25:05 -0300

    feat: bearer token

--- src/shared/api/marketplace.ts
+++ src/shared/api/marketplace.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const getBaseUrl = () => {
     // API roda em outro PC na mesma rede, então usamos o IP de LAN
@@ -6,7 +7,7 @@ const getBaseUrl = () => {
     return "http://192.168.0.236:3001"
 }
 
-const baseURL = getBaseUrl()
+export const baseURL = getBaseUrl()
 
 export class MarketPlaceApiClient {
 
@@ -17,11 +18,35 @@ export class MarketPlaceApiClient {
         this.instance = axios.create({
             baseURL,
         })
+        this.setupInterceptors()
     }
 
     getInstance() {
         return this.instance
     }
+
+    private setupInterceptors() {
+        
+        this.instance.interceptors.request.use(async (config) => {
+
+            const userData = await AsyncStorage.getItem("marketplace-auth")
+            
+            if(userData) {
+                const {state: {
+                    token
+                } } = JSON.parse(userData)
+
+                if(token){
+                    config.headers.Authorization = `Bearer ${token}`
+                }
+            }
+
+            return config
+        }, (error) => {
+            return Promise.reject(error)
+        })
+    }
+
 }
 
 export const marketPlaceApi = new MarketPlaceApiClient().getInstance()
--- src/shared/services/auths.service.ts
+++ src/shared/services/auths.service.ts
@@ -1,7 +1,8 @@
-import { marketPlaceApi } from "../api/marketplace";
+import { baseURL, marketPlaceApi } from "../api/marketplace";
 import { AuthResponse } from "../interface/http/auth-response";
 import { LoginHTTPParams } from "../interface/http/login";
 import { RegisterHTTPParams } from "../interface/http/register";
+import { UploadAvatarResponse } from "../interface/http/upload-avatar";
 
 export const register = async (userData: RegisterHTTPParams) => {
 
@@ -16,6 +17,22 @@ export const register = async (userData: RegisterHTTPParams) => {
 export const login = async (userData: LoginHTTPParams) => {
 
     const { data } = await marketPlaceApi.post<AuthResponse>("/auth/login", userData)
+
+    return data
+}
+
+export const uploadAvatar = async (avatarURI: string) => {
+    const formData = new FormData()
+
+    formData.append("avatar", {
+        uri: avatarURI,
+        type: "image/jpeg",
+        name: "acatar.jpeg"
+    } as unknown as Blob)
+
+    const { data } = await marketPlaceApi.post<UploadAvatarResponse>("/user/avatar")
+    
+    data.url = `${baseURL}${data.url}`
 
     return data
 }
